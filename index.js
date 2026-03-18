aef6c89be59a1125099cd14d96f81bca4d28fff0
leopii98 <rippergames98@gmail.com>
2026-03-18 17:14:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log("Benvenuto nel mondo di leopii!!!")
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Express ready!", 3000)
+});
