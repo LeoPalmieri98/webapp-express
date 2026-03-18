4bb106b0ff1f5efd94d95438feacb5ffd9ddf893
leopii98 <rippergames98@gmail.com>
2026-03-18 19:45:49 +0100

    add.controller.show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -4,7 +4,7 @@ const db = require("../data/db")
 
 const index = (req, res) => {
 
-    const sqlQuery = "SELECT * FROM db_movies.movies; ";
+    const sqlQuery = "SELECT * FROM movies; ";
     db.query(sqlQuery, (err, results) => {
         if (err) return res.status(500).json({
             error: "Error database server", message: "Database query failed"
@@ -12,20 +12,53 @@ const index = (req, res) => {
         });
 
         res.json(results);
-
-
     })
-
-
-
-
-
 };
 
 
-function show(req, res) {
+const show = (req, res) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "User Error", message: "ID non trovato" });
+    }
+    const entityQuery = "SELECT * FROM movies WHERE id = ?";
+    const relationsQuery = `
+           SELECT * 
+           FROM movies
+            JOIN reviews
+            ON movies.id = reviews.movie_id  
+            WHERE reviews.movie_id   =  ?;
+	        `
 
+    const parametriQuery = [id];
+
+    db.query(entityQuery, parametriQuery, (error, results) => {
+
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: "Query error", message: "Impossibile processare la richiesta" });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: "Not found", message: "Impossibile trovare la risorsa richiesta" });
+        }
+
+        const post = results[0];
+        console.log("post", post);
+
+        db.query(relationsQuery, parametriQuery, (error, results) => {
+
+            if (error) {
+                return res.status(500).json({ error: "Query error", message: "Impossibile processare la richiesta" });
+            }
+
+            res.json(post);
+
+        });
+
+    });
 };
+
 
 const controllers = {
     index,
