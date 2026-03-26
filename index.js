9f618d7220d76d264247e601d3b07a6f562f22d8
leopii98 <rippergames98@gmail.com>
2026-03-26 18:57:12 +0100

    fix-error

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 
 const moviesRouter = require("./routers/moviesRouter.js")
 const notFoundMiddleware = require("./middlewares/notFound");
 const errorsHandlerMiddleware = require("./middlewares/errorsHandler.js");
 
-
-app.use(express.static("public"));
+app.use(cors({ origin: process.env.FE_URL }));
+app.use("/static/", express.static("public"));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome-ato our server")
+    res.send("Welcome to our server")
 });
 app.use("/movies", moviesRouter);
 app.use(notFoundMiddleware);
